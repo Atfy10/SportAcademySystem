7efa9ef722cef25e700c7617530dadbc23088729
Abdulrahman Al-Atfy <abdo.mo.atfy789@gmail.com>
2025-09-19 15:10:26 +0300

    add Notification entity with its relations

---
+++ SportAcademy.Domain/Entities/NotificationRecipient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportAcademy.Domain.Entities
+{
+    public class NotificationRecipient
+    {
+        public bool IsRead { get; set; }
+        public int NotificationId { get; set; }
+        public required string UserId { get; set; }
+
+        // Navigation Property
+        public virtual AppUser User { get; set; } = null!;
+        public virtual Notification Notification { get; set; } = null!;
+
+    }
+}
