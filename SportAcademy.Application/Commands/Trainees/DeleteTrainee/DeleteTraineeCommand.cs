054864ff41a3c9640472b4f850f6ebe8c0c26b9a
Abdulrahman Al-Atfy <abdo.mo.atfy789@gmail.com>
2025-09-20 08:33:56 +0300

    implement delete trainee command

---
+++ SportAcademy.Application/Commands/Trainees/DeleteTrainee/DeleteTraineeCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SportAcademy.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportAcademy.Application.Commands.Trainees.DeleteTrainee
+{
+    public record DeleteTraineeCommand(int Id) : IRequest<Result<bool>>;
+}
---
+++ SportAcademy.Application/Commands/Trainees/DeleteTrainee/DeleteTraineeCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SportAcademy.Application.Interfaces;
+using SportAcademy.Application.Services;
+using SportAcademy.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportAcademy.Application.Commands.Trainees.DeleteTrainee
+{
+    public class DeleteTraineeCommandHandler : IRequestHandler<DeleteTraineeCommand, Result<bool>>
+    {
+        private readonly ITraineeRepository _traineeRepository;
+        private readonly string _operation = OperationType.Delete.ToString();
+        public DeleteTraineeCommandHandler(ITraineeRepository traineeRepository)
+        {
+            _traineeRepository = traineeRepository;
+        }
+        public async Task<Result<bool>> Handle(DeleteTraineeCommand request, CancellationToken cancellationToken)
+        {
+            if(request.Id <= 0)
+                throw new ArgumentException("Invalid Id");
+
+            await _traineeRepository.DeleteAsync(request.Id, cancellationToken);
+            return Result<bool>.Success(true, _operation);
+        }
+    }
+}
