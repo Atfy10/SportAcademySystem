8a74d394ac6ac0f12056a2904213928e86f83fea
Atfy10 <136708389+Atfy10@users.noreply.github.com>
2026-02-10 03:16:28 +0200

    first try

--- SportAcademy.Web/Controllers/ChatBotController.cs
+++ SportAcademy.Web/Controllers/ChatBotController.cs
@@ -7,5 +7,14 @@ namespace SportAcademy.Web.Controllers
     [ApiController]
     public class ChatBotController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult GetResponse([FromBody] string userInput)
+        {
+            // Here you would typically call your chatbot service to get a response based on the user input.
+            // For demonstration purposes, we'll return a simple response.
+            string botResponse = $"You said: {userInput}. This is a response from the chatbot.";
+            return Ok(botResponse);
+        }
+
     }
 }
