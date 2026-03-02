0f137cd0846533572c398d1025abd6529e5f5a6f
Atfy10 <136708389+Atfy10@users.noreply.github.com>
2026-03-02 14:27:02 +0200

    init dashboard controller

---
+++ SportAcademy.Web/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportAcademy.Web.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+    }
+}
