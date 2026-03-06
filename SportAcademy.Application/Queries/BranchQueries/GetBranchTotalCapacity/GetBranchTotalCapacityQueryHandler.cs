2a416f524362d059e13e0a454763dad7c2865bd7
Coach Search Feature <coach-search@example.com>
2026-03-06 06:39:57 +0200

    guard

--- SportAcademy.Application/Queries/BranchQueries/GetBranchTotalCapacity/GetBranchTotalCapacityQueryHandler.cs
+++ SportAcademy.Application/Queries/BranchQueries/GetBranchTotalCapacity/GetBranchTotalCapacityQueryHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using SportAcademy.Application.Common.Result;
 using SportAcademy.Application.Interfaces;
 using SportAcademy.Domain.Exceptions.BaseExceptions;
+using SportAcademy.Domain.Exceptions.BranchExceptions;
 
 public class GetBranchTotalCapacityQueryHandler
     : IRequestHandler<GetBranchTotalCapacityQuery, Result<int>>
@@ -18,6 +19,10 @@ public class GetBranchTotalCapacityQueryHandler
         GetBranchTotalCapacityQuery request,
         CancellationToken cancellationToken)
     {
+        var isExist = await _branchRepository.IsExistAsync(request.BranchId, cancellationToken);
+        if (!isExist)
+            throw new BranchNotFoundException(request.BranchId.ToString());
+
         var totalCapacity = await _branchRepository
             .GetBranchTotalCapacityAsync(request.BranchId, cancellationToken);
 
