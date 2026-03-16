f6d44ed733b9f814cd1d4fbece817b4fbf3ac0f4
Atfy10 <136708389+Atfy10@users.noreply.github.com>
2026-03-16 17:01:40 +0200

    seperate dto from repo

--- SportAcademy.Application/Interfaces/INotificationRepository.cs
+++ SportAcademy.Application/Interfaces/INotificationRepository.cs
@@ -1,10 +1,6 @@
 using SportAcademy.Application.Common.Pagination;
+using SportAcademy.Application.DTOs.NotificationsDtos;
 using SportAcademy.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SportAcademy.Application.Interfaces
 {
@@ -15,16 +11,5 @@ namespace SportAcademy.Application.Interfaces
         Task<int> GetUnreadCountAsync(string userId, CancellationToken ct = default);
         Task<bool> MarkAsReadAsync(int notificationId, string userId, CancellationToken ct = default);
         Task<int> MarkAllAsReadAsync(string userId, CancellationToken ct = default);
-    }
-
-    public class NotificationRecipientDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Message { get; set; } = string.Empty;
-        public string Type { get; set; } = "system";
-        public string? ActionUrl { get; set; }
-        public bool IsRead { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
